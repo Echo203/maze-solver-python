9798763091a82b0d762b01c8ab10cdf65e83199f
Echo203 <tksd97@wp.pl>
2026-02-07 21:05:03 +0100

    Initial commit, window class basic setup

---
+++ window.py
@@ -0,0 +1,25 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window:
+    def __init__(self, width, height, title):
+        self.width = width
+        self.height = height
+        self.root = Tk()
+        self.root.title = title
+        self.canvas = Canvas()
+        self.canvas.pack()
+        self.running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.root.update()
+        self.root.update_idletasks()
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+
+    def close(self):
+        self.running = False
