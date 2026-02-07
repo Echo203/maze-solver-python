3bfabf9f7b2c42b892242053fe5cf96cda60ccce
Echo203 <tksd97@wp.pl>
2026-02-07 23:01:47 +0100

    removed print statements

--- line.py
+++ line.py
@@ -16,6 +16,3 @@ class Line:
             fill=color,
             width=2,
         )
-        print(f"DRAWING {color.capitalize()} LINE:")
-        print(f"from x: {self.point_one.x} y: {self.point_one.y}")
-        print(f"to   x: {self.point_two.x} y: {self.point_two.y}")
