846246b1eb4b267101d6110035c529e9577a3741
Echo203 <tksd97@wp.pl>
2026-02-07 22:06:36 +0100

    Cells setup

--- cell.py
+++ cell.py
@@ -1,5 +1,6 @@
 from window import Window
 from helpers import create_line_with_coordinates
+from typing import Self
 
 
 class Cell:
@@ -14,6 +15,12 @@ class Cell:
         self.__y2 = -1
         self.__win = window
 
+    def get_coords(self):
+        return [self.__x1, self.__y1, self.__x2, self.__y2]
+
+    def get_center(self):
+        return [int((self.__x1 + self.__x2) / 2), int((self.__y1 + self.__y2) / 2)]
+
     def draw(self, x1: int, y1: int, x2: int, y2: int):
         self.__x1 = x1
         self.__y1 = y1
@@ -21,13 +28,27 @@ class Cell:
         self.__y2 = y2
 
         if self.has_bottom_wall:
-            self.__win.draw_line(create_line_with_coordinates(x1, y1, x1, y2))
+            self.__win.draw_line(create_line_with_coordinates(x1, y2, x2, y2))
 
         if self.has_left_wall:
-            self.__win.draw_line(create_line_with_coordinates(x1, y1, x2, y1))
+            self.__win.draw_line(create_line_with_coordinates(x1, y1, x1, y2))
 
         if self.has_top_wall:
-            self.__win.draw_line(create_line_with_coordinates(x2, y1, x2, y2))
+            self.__win.draw_line(create_line_with_coordinates(x1, y1, x2, y1))
 
         if self.has_right_wall:
-            self.__win.draw_line(create_line_with_coordinates(x1, y2, x2, y2))
+            self.__win.draw_line(create_line_with_coordinates(x2, y1, x2, y2))
+
+    def draw_move(self, to_cell: Self, undo=False):
+        color = "red"
+        if undo:
+            color = "gray"
+
+        center_one = self.get_center()
+        center_two = to_cell.get_center()
+        self.__win.draw_line(
+            create_line_with_coordinates(
+                center_one[0], center_one[1], center_two[0], center_two[1]
+            ),
+            color,
+        )
--- main.py
+++ main.py
@@ -17,7 +17,9 @@ win = Window(SCREEN_WIDTH, SCREEN_HEIGHT, "XD")
 cell_one = Cell(win)
 cell_two = Cell(win)
 
-cell_one.draw(10, 10, 20, 20)
+cell_one.draw(10, 10, 50, 50)
 cell_two.draw(50, 50, 80, 80)
+
+cell_one.draw_move(cell_two)
 
 win.wait_for_close()
